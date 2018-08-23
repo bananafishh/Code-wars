fcf8efcdc33dda6d4531315f3b819b9bc5b7027e
bananafishh <z.dyshekova@gmail.com>
2018-08-24 00:08:58 +0300

    Оптимизация обнуления переменной с пустой строкой
    Перенесла объявление переменной repeated внутрь метода forEach, чтобы
    она обнулялась в начале каждой итерации по массиву естественным образом.

--- mumbling/script.js
+++ mumbling/script.js
@@ -1,17 +1,17 @@
 function accum(s) {
   var arr = s.toUpperCase().split('');
-  var repeated = '';
   var newArr = [];
 
   arr.forEach(function(item, i) {
+    var repeated = '';
     var n = 0;
+
     while (n <= i) {
       n === 0 ? repeated += item : repeated += item.toLowerCase();
       n++;
     }
 
     newArr.push(repeated);
-    repeated = '';
   });
 
   return newArr.join('-');
