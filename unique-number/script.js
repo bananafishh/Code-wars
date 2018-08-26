f750c4b53773d08d85278cc6a119da68dc9815e4
bananafishh <z.dyshekova@gmail.com>
2018-08-26 04:34:56 +0300

    ES6, детка
    Заменила цикл for циклом for... of, так как сразу получается число для
    сравнения без необходимости обращения к массиву по индексу.

--- unique-number/script.js
+++ unique-number/script.js
@@ -1,13 +1,13 @@
 function findUniq(arr) {
-  var firstNum = arr[0];
+  const firstNum = arr[0];
 
   if (firstNum != arr[1] && firstNum != arr[2]) {
     return firstNum;
   }
 
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] != firstNum) {
-      return arr[i];
+  for(let i of arr) {
+    if(i != firstNum) {
+      return i;
     }
   }
 }
