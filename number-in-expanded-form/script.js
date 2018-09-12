82a96d3d8c82405825846474b6a083467b82f076
bananafishh <z.dyshekova@gmail.com>
2018-09-12 04:05:39 +0300

    Форматирование кода после тестирования с eslint

--- number-in-expanded-form/script.js
+++ number-in-expanded-form/script.js
@@ -1,12 +1,14 @@
+'use strict';
+
 function expandedForm(num) {
   const arr = num.toString().split('');
   const result = [];
   let expandedNum;
 
-  for (let i = 0, arrLength = arr.length;  i < arrLength; i++) {
+  for (let i = 0, arrLength = arr.length; i < arrLength; i++) {
     expandedNum = arr[i] * Math.pow(10, arrLength - i - 1);
 
-    if(expandedNum) {
+    if (expandedNum) {
       result.push(expandedNum);
     }
   }
