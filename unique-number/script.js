6afee7df3a8412aeec8059db6a455e5f1b3a5dba
bananafishh <z.dyshekova@gmail.com>
2018-09-12 04:37:16 +0300

    Возвращение значения непосредственно из функции

--- unique-number/script.js
+++ unique-number/script.js
@@ -1,13 +1,20 @@
+'use strict';
+
 function findUniq(arr) {
   const firstNum = arr[0];
 
-  if (firstNum != arr[1] && firstNum != arr[2]) {
+  if (firstNum !== arr[1] && firstNum !== arr[2]) {
     return firstNum;
   }
 
-  for(let i of arr) {
-    if(i != firstNum) {
-      return i;
+  for (let i of arr) {
+    if (i !== firstNum) {
+      var uniqueNumber = i;
+      break;
     }
   }
+
+  return uniqueNumber;
 }
+
+console.log(findUniq([1, 1, 1, 2, 1, 1]));
